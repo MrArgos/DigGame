058257603d3d26be662eb97c119baeae739e8fd4
João Costa <joao.pmc20@gmail.com>
2021-06-19 05:59:20 +0100

    Done

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -10,79 +10,42 @@ document.body.appendChild(renderer.domElement);
 var geometria = new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshPhongMaterial({color : 0xff0000});
 var cubo = new THREE.Mesh(geometria, material);
-var cuboCoordRotation;
+var clock = new THREE.Clock();
+
 var camaraCoord;
-var velocidadeAndar = 0.05;
 var activaCamaraPerspetiva = true;
 var dirLight;
-
-
-var objetoImportado;
-var mixerAnimacao;
-var relogio = new THREE.Clock();
-var importer = new THREE.FBXLoader();
 var followSpotLight;
-var rowSize = 16;
-var heigthSize = 8;
 var cubes = new THREE.Object3D();
 var robotBody = new THREE.Object3D();
+var laserPosition;
+var laserDir = -1;
+var rowSize = 16;
+var heigthSize = 8;
 var initialPos = {x:(rowSize/2), y:0};
 var playerPosition = {x:initialPos.x, y:initialPos.y};
-var laserDir = -1;
-var laserPosition;
 var score = 0;
+var animationTimer = 0;
+var laserBeam;
+var activeLaserBeamAnimation;
+var menuOpen = false;
 
+// carregar texturas
 var textureLoader = new THREE.TextureLoader();
-var dirt1Texture = textureLoader.load("Textures/TextureGround2.jpg");
-var dirt1BumpMap = textureLoader.load("Textures/TextureGround2Normal.jpg");
+var dirtTexture = textureLoader.load("Textures/Ground/TextureGround2.jpg");
+var dirtNormal = textureLoader.load("Textures/Ground/TextureGround2Normal.jpg");
+var ironTexture = textureLoader.load("Textures/Ground/Rock_Ore_001_COLOR.jpg");
+var ironNormal = textureLoader.load("Textures/Ground/Rock_Ore_001_NORM.jpg");
+var robot1Texture = textureLoader.load("Textures/Robot/Metal_Rusted_008_basecolor.jpg");
+var robot1Normal = textureLoader.load("Textures/Robot/Metal_Rusted_008_normal.jpg");
+var robot2Texture = textureLoader.load("Textures/Robot/Greeble_Techno_002_basecolor.jpg");
+var robot2Normal = textureLoader.load("Textures/Robot/Greeble_Techno_002_normal.jpg");
+var laserTexture = textureLoader.load("Textures/Robot/Sci-fi_Wall_009_basecolor.jpg");
+var laserNormal = textureLoader.load("Textures/Robot/Sci-fi_Wall_009_normal.jpg");
 
-importer.load('./Objetos/Samba Dancing.fbx', function (object) {
-
-    mixerAnimacao = new THREE.AnimationMixer(object);
-    var action = mixerAnimacao.clipAction(object.animations[0]);
-    action.play();
-
-    object.traverse(function (child) {
-        if (child.isMesh) {
-            child.castShadow = true;
-            child.recieveShadow = true;
-        }
-    });
-
-    //cena.add(object);
-
-    object.scale.x = 0.005;
-    object.scale.z = 0.005;
-    object.scale.y = 0.005;
-
-    object.position.z = 0.5;
-    //object.position.y = -0.5;
-
-    objetoImportado = object;
-    objetoImportado.castShadow = true;
-    //objetoImportado.add(camara);
-});
-
-document.addEventListener('mousemove', ev =>{
-    var x = (ev.clientX - 0) / (window.innerWidth - 0) * (1 - (-1)) + -1;
-    var y = (ev.clientY - 0) / (window.innerHeight -0) * (1 - (-1)) + -1;
-
-    //camaraCoord = {x:x, y:y};
-    cuboCoordRotation = {x:x, y:y};
-});
 
 document.addEventListener('keydown', ev =>{
     
-    // if (ev.keyCode == 87) // W
-    // {
-    //     playerPosition.y++;
-    //     camaraCoord.y++;
-    // }
-    if (ev.keyCode == 83) // S
-    {    
-        moveDown();
-    }
-
     if (ev.keyCode == 65) // A
     {
         moveLeft();
@@ -94,35 +57,41 @@ document.addEventListener('keydown', ev =>{
     }
 
     if (ev.keyCode == 67) // C
-    { 
+    {   // Trocar entre camara Perspetiva e Ortografica
         activaCamaraPerspetiva = !activaCamaraPerspetiva;
     }
 
     if (ev.keyCode == 76) // L
-    {
+    {   // Trocar entre DirectionaLight e SpotLight
         dirLight.visible = !dirLight.visible;
         followSpotLight.visible = !followSpotLight.visible;
     }
 
     if (ev.keyCode == 32) // Espaço
-    {
+    {   // Disparar o laser / destruir o bloco
         destroyBlock();
     } 
 
     if (ev.keyCode == 37) // left arrow
-    {
+    {   // mover o laser para a esquerda
         moveLaserLeft();
     } 
 
     if (ev.keyCode == 39) // right arrow
-    {
+    {   // mover o laser para a direita
         moveLaserRight();
     } 
+
+    if(ev.keyCode == 72) // H
+    {
+        menuAjuda();
+    }
 
 });
 
 function Start() {
     var mult = 80;
+    // Camara Perspetiva e Ortografica
     camaraPerspetiva = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
     camaraOrtografica = new THREE.OrthographicCamera( window.innerWidth / (- 2 * mult), window.innerWidth / (2* mult), 
         window.innerHeight / (2 * mult), window.innerHeight / (- 2 * mult), 1, 5 );
@@ -138,7 +107,7 @@ function Start() {
     camaraOrtografica.position.z = 2;
 
     // SpotLight que segue o robot
-    followSpotLight = new THREE.SpotLight('#909040', 0.8);
+    followSpotLight = new THREE.SpotLight(0x969696, 1);
     followSpotLight.position.x = initialPos.x;
     followSpotLight.position.y = initialPos.y;
     followSpotLight.position.z = 1.5;
@@ -149,7 +118,7 @@ function Start() {
     cena.add(followSpotLight.target);
 
     // Directional Light
-    dirLight = new THREE.DirectionalLight(0x909040, 0.8);
+    dirLight = new THREE.DirectionalLight(0x969696, 1);
     dirLight.castShadow = true;
     dirLight.position.set(0,2,3);
     cena.add(dirLight);
@@ -158,44 +127,56 @@ function Start() {
     // Gerar cubos (chao)
     generateCubes();
     cena.add(cubes);
-    for (let i = 0; i < cubes.children.length; i++) {
-        //console.log(cubes.children[i].name);
-    }
 
+    // contruir o robot
     buildRobot();
     requestAnimationFrame(update);
 }
 
 function update() {
-    
-    if (cuboCoordRotation != null) {
-        cubo.rotation.x += cuboCoordRotation.y * 0.1;
-        cubo.rotation.y += cuboCoordRotation.x * 0.1;
-    }
-
-    if (mixerAnimacao) {
-        mixerAnimacao.update(relogio.getDelta());
-    }
 
     if(robotBody != null)
     {
+        // atualizar a posição do robot
         robotBody.position.lerp(new THREE.Vector3(playerPosition.x, playerPosition.y, robotBody.position.z), 0.03);
+        // atualizar a posiçao da luz e do target da luz
         followSpotLight.target.position.lerp(new THREE.Vector3(playerPosition.x, playerPosition.y, robotBody.position.z), 0.03);
         followSpotLight.position.lerp(new THREE.Vector3(playerPosition.x, playerPosition.y, followSpotLight.position.z), 0.03);
+        // atualizar a posiçao da camera
         camaraPerspetiva.position.lerp(new THREE.Vector3(camaraCoord.x, camaraCoord.y, camaraPerspetiva.position.z), 0.015);
 
+        // atualizar a posição do laser
         var laser = robotBody.getObjectByName("laser");
         if(laser != null && laserPosition != null) {
             laser.position.lerp(new THREE.Vector3(laserPosition.x, laserPosition.y, laser.position.z), 0.1);
         }
     }
 
+    // mudar de camara
     if (activaCamaraPerspetiva) {
         renderer.render(cena, camaraPerspetiva);
     }
     else {
         renderer.render(cena, camaraOrtografica);
     }
+
+    // corre a animaçao do feixe de laser durante 1 segundo
+    if (laserBeam != null) {
+        if (activeLaserBeamAnimation && animationTimer <= 1) {
+            laserBeam.visible = true;
+            animationTimer += clock.getElapsedTime();
+        }else {
+            if (activeLaserBeamAnimation && laserDir == 0) {
+                laserBeam.rotateZ(Math.PI/2);
+            }
+            laserBeam.visible = false;
+            animationTimer = 0;
+            activeLaserBeamAnimation = false;
+            clock.stop();
+        }
+    }
+
+    console.log();
     
     requestAnimationFrame(update);
 }
@@ -207,7 +188,7 @@ function generateCubes() {
     for (let i = 0; i >= (-heigthSize)-1; i--) {
         for (let j = -1; j <= rowSize ; j++) {
             var prob = Math.random();
-            if (i == 0) {
+            if (i == 0) { // gerar uma barreira indestrutivel à volta da área de jogo
                 newMat = new THREE.MeshPhongMaterial({ color: 0x000000});
                 newCube = new THREE.Mesh(geometria, newMat);
                 newCube.matDefinido = "Indestrutivel";
@@ -222,43 +203,49 @@ function generateCubes() {
                 newCube.matDefinido = "Indestrutivel";
                 console.log(i,j);
             }
-            else if (prob > 0.3) {
-                newMat = new THREE.MeshPhongMaterial({map: dirt1Texture, normalMap: dirt1BumpMap, shininess: 10});
+            else if (prob > 0.3) {  // 70% probabilidade de ser terra
+                newMat = new THREE.MeshPhongMaterial({
+                    map: dirtTexture, 
+                    normalMap: dirtNormal, 
+                    shininess: 10});
                 newCube = new THREE.Mesh(geometria, newMat);
-                newCube.matDefinido = "Terra";
+                newCube.matDefinido = "Dirt";
             }
-            else if (prob > 0.1){
-                newMat = new THREE.MeshPhongMaterial({ color: 0xff0000});
+            else if (prob > 0.1){   // 20% probabilidade de ser ferro
+                newMat = new THREE.MeshPhongMaterial({ 
+                    map: ironTexture, 
+                    normalMap: ironNormal});
                 newCube = new THREE.Mesh(geometria, newMat);
-                newCube.matDefinido = "Ferro";
-            }else{
+                newCube.matDefinido = "Ore";
+            }else{                 // 10% probabilidade de ser indestrutivel
                 newMat = new THREE.MeshPhongMaterial({ color: 0x000000});
                 newCube = new THREE.Mesh(geometria, newMat);
                 newCube.matDefinido = "Indestrutivel";
             }
             newCube.receiveShadow = true;
-            newCube.position.x = j;
+            newCube.position.x = j; // atualizar a posição do cubo
             newCube.position.y = i;
-            newCube.name = j + ',' + i;
+            // dar um nome com a posiçao para depois poder
+            // procurar pelo nome para destruir
+            newCube.name = j + ',' + i; 
             cubes.add(newCube);
         }   
     }
 }
 
 function buildRobot() {
-    // Robot 
-    // main body
-    var robotMainMat = new THREE.MeshPhongMaterial({color : 0xff0000});
-    //var robotMainMat = new THREE.MeshPhongMaterial({map : metalTexture});
+    // Robot
+    var robotMainMat = new THREE.MeshPhongMaterial({map : robot2Texture, normalMap: robot2Normal});
+    robotMainMat.metalness = 1;
     var robotMainGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.2, 8);
     var robotMain = new THREE.Mesh(robotMainGeometry, robotMainMat);
     robotMain.castShadow = true;
     robotMain.receiveShadow = true;
     robotMain.rotation.set(Math.PI / 2, Math.PI /8, 0);
     robotBody.add(robotMain);
-    
-    // legs
-    var robotLegsMat = new THREE.MeshPhongMaterial({color : 0x00FF00});
+
+    // pernas
+    var robotLegsMat = new THREE.MeshPhongMaterial({map : robot1Texture, normalMap: robot1Normal});
     var robotLegsGeo = new THREE.BoxGeometry(0.2, 0.3, 0.1);
     var robotLegLeft = new THREE.Mesh(robotLegsGeo, robotLegsMat);
     var robotLegRight = new THREE.Mesh(robotLegsGeo, robotLegsMat);
@@ -273,7 +260,7 @@ function buildRobot() {
     robotBody.add(robotLegLeft);
     robotBody.add(robotLegRight);
 
-    // feet
+    // pés
     var feetShape = new THREE.Shape();
     feetShape.moveTo(0.1,0);
     feetShape.lineTo(0.5,0);
@@ -290,10 +277,8 @@ function buildRobot() {
         bevelSize: 0.1,  
         bevelSegments: 8
     };
-    //var robotFeetGeo = new THREE.ShapeGeometry(feetShape);
     var robotFeetGeo = new THREE.ExtrudeGeometry(feetShape, extrudeSettings);
     var robotFeetMat = new THREE.MeshPhongMaterial({color : 0x292929});
-    //var robotFeetGeo = new THREE.CylinderGeometry(0.3, 0.3, 0.1, 3);
     var robotFootLeft = new THREE.Mesh(robotFeetGeo, robotFeetMat);
     var robotFootRight = new THREE.Mesh(robotFeetGeo, robotFeetMat);
     robotFootLeft.position.set(-0.58, -0.6, 0.1);
@@ -308,9 +293,40 @@ function buildRobot() {
     robotBody.add(robotFootLeft);
     robotBody.add(robotFootRight);
     
+    // rodas
+    var robotFeetWheelsGeo = new THREE.CylinderGeometry(0.07,0.07,0.005,10);
+    var robotFeetWheelsMat = new THREE.MeshPhongMaterial({map : robot1Texture, normalMap: robot1Normal});
+    var robotFeetWheels = new THREE.Mesh(robotFeetWheelsGeo, robotFeetWheelsMat);
+    robotFeetWheels.position.x = -0.53;
+    robotFeetWheels.position.y = -0.53;
+    robotFeetWheels.position.z = 0.2;
+    robotFeetWheels.rotateX(Math.PI/2);
+    robotFeetWheels.castShadow = true;
+    robotBody.add(robotFeetWheels);
+    robotFeetWheels = robotFeetWheels.clone();
+    robotFeetWheels.position.x = -0.29;
+    robotFeetWheels.position.y = -0.53;
+    robotBody.add(robotFeetWheels);
+    robotFeetWheels = robotFeetWheels.clone();
+    robotFeetWheels.position.x = -0.53;
+    robotFeetWheels.position.y = -0.3;
+    robotBody.add(robotFeetWheels);
+    robotFeetWheels = robotFeetWheels.clone();
+    robotFeetWheels.position.x = +0.53;
+    robotFeetWheels.position.y = -0.53;
+    robotBody.add(robotFeetWheels);
+    robotFeetWheels = robotFeetWheels.clone();
+    robotFeetWheels.position.x = +0.29;
+    robotFeetWheels.position.y = -0.53;
+    robotBody.add(robotFeetWheels);
+    robotFeetWheels = robotFeetWheels.clone();
+    robotFeetWheels.position.x = +0.53;
+    robotFeetWheels.position.y = -0.3;
+    robotBody.add(robotFeetWheels);
+
     // laser
     var robotLaserGeo = new THREE.ConeGeometry(0.1, 0.25, 20);
-    var robotLaserMat = new THREE.MeshPhongMaterial({color : 0x0000ff});
+    var robotLaserMat = new THREE.MeshPhongMaterial({map : laserTexture, normalMap: laserNormal});
     var robotLaser = new THREE.Mesh(robotLaserGeo, robotLaserMat);
     robotLaser.position.set(-0.55,0,0);
     robotLaser.rotation.set(0,0, Math.PI / 2);
@@ -319,55 +335,53 @@ function buildRobot() {
     robotLaser.name = "laser";
     robotBody.add(robotLaser);
     
-    //console.log("initialPos:{0}",initialPos);
+    // laserbeam 
+    laserBeam = LaserBeam();
+    laserBeam.visible=false;
+    robotBody.add(laserBeam);
+
     robotBody.scale.set(0.8,0.8,0.8);
     robotBody.position.set(initialPos.x, 0, 0);
     cena.add(robotBody);
 }
 
-function moveLeft(){
+function moveLeft(){    // verificar se tem algum cubo à esquerda
     var objName = (playerPosition.x - 1).toString() + "," + playerPosition.y.toString();
-    //console.log(objName);
     var c = cubes.getObjectByName(objName);
-    //console.log(c);
-    if (c == null || c.visible == false) {
+    if (c == null || c.visible == false) { // se não existir cubo ou estiver escondido, mover
         playerPosition.x--;
         camaraCoord.x--;
     }
 }
 
-function moveRight(){
+function moveRight(){   
     var objName = (playerPosition.x + 1).toString() + "," + playerPosition.y.toString();
-    //console.log(objName);
     var c = cubes.getObjectByName(objName);
-    //console.log(c);
     if (c == null || c.visible == false) {
         playerPosition.x++;
         camaraCoord.x++;
     }
 }
 
-function moveDown(){
+function moveDown(){    
     var objName = (playerPosition.x).toString() + "," + (playerPosition.y -1).toString();
-    //console.log(objName);
     var c = cubes.getObjectByName(objName);
-    //console.log(c);
-    if (c == null || c.visible == false) {
+    if (c == null || c.visible == false) { 
         playerPosition.y--;
         camaraCoord.y--;
     }
 }
 
 function moveLaserLeft(){
-    if (laserDir != -1) {
+    if (laserDir != -1) {   // verificar a posiçao atual do laser
         laserDir--;
         var laser = robotBody.getObjectByName("laser");
         laserPosition = {x:laser.position.x, y:laser.position.y};
-        laserPosition.x -= 0.55;
+        laserPosition.x -= 0.55; // mover para a esquerda
         laser.rotateZ(-Math.PI/2);
-        if (laserDir == 0) {
+        if (laserDir == 0) {    // se o laser ficar em baixo, mover para baixo
             laserPosition.y -= 0.55;
-        }else {
+        }else {                 // se ficar em cima(lado), mover para cima
             laserPosition.y += 0.55;
         }
     }
@@ -390,30 +404,57 @@ function moveLaserRight(){
 }
 
 function destroyBlock(){
-    var objName;
-    if (laserDir == 0) {
-        objName = (playerPosition.x).toString() + "," + (playerPosition.y -1).toString();
-    }
-    else {
-        objName = (playerPosition.x + laserDir).toString() + "," + (playerPosition.y).toString();
-    }
-
+    // procurar o objeto que esteja à esquerda, direita ou por baixo do robot,
+    // consoante a direçao que esteja a apontar o laser
+    var objName = (playerPosition.x + laserDir).toString() 
+                + "," + (playerPosition.y - (1- Math.abs(laserDir))).toString();
     var c = cubes.getObjectByName(objName);
-        if (c != null && c.visible != false) {
+        if (c != null && c.visible != false) { // se o cubo existir e não estiver invisivel (ou seja, destruído)
             switch (c.matDefinido) {
-                case 'Terra':
+                case 'Dirt':
                     score +=10;
                     break;
-                case 'Ferro':
-                    score+=30;
+                case 'Ore':
+                    score +=50;
                     break;
-                case 'Indestrutivel':
+                case 'Indestrutivel': // se for indestrutivel, nao fazer nada
                     return;  
             }
-            c.visible = false;
-            var text = document.getElementById("p").innerHTML = score;
-            if (laserDir == 0) {
+            c.visible = false;      // caso seja terra ou minério, destruir o cubo
+            var text = document.getElementById("p").innerHTML = score;  // aumentar o score
+            if (laserDir == 0) {    // caso se destrua o objeto por baixo, mover para baixo
                 moveDown();
             }
+            showLaserBeam();
         }
+}
+
+function showLaserBeam(){
+    switch (laserDir) {
+        case -1:
+            laserBeam.position.x = -1.5;
+            laserBeam.position.y = 0;
+            break;
+        case 0:
+            laserBeam.position.x = 0;
+            laserBeam.rotateZ(-Math.PI / 2);
+            laserBeam.position.y = -0.3;
+            break;
+        case 1:
+            laserBeam.position.x = 0.5;
+            laserBeam.position.y = 0;
+            break;
+    }
+    clock.start();
+    activeLaserBeamAnimation = true;
+}
+
+function menuAjuda(){
+    if (!menuOpen) {
+        document.getElementById("ajuda_open").style.display = 'block';
+        document.getElementById("ajuda_closed").style.display = 'none';
+    }else{
+        document.getElementById("ajuda_open").style.display = 'none';
+    }
+    menuOpen = !menuOpen;
 }
